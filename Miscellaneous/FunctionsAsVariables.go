ab594b75d9feba40de46cebd12460769110cc400
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-04-30 14:06:52 +0100

    Function as First-Class Values

---
+++ Miscellaneous/FunctionsAsVariables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var funcVar func(int) int
+
+func incFn(x int) int {
+	return x + 1
+}
+
+func applyIt(funcArg func(int) int, para int) int {
+	return funcArg(para)
+}
+
+func main() {
+	funcVar = incFn
+	fmt.Println(funcVar(1))
+
+	fmt.Println(applyIt(incFn, 2))
+}

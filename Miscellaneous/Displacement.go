5c3b6fe8f01625b4d8052e376817b0401316e65f
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-04-30 15:33:19 +0100

    Reusable code for User Prompting

--- Miscellaneous/Displacement.go
+++ Miscellaneous/Displacement.go
@@ -8,23 +8,12 @@ import (
 func main() {
 	acc, vel, dis := PromptUserEquation()
 	var computeDisplacement = GenDisplaceFn(acc, vel, dis)
-	time := PromptUserTime()
 
+	// We can make a loop just asking about time here...
+	var time = funcName("Time")
 	fmt.Print("The displacement is: ")
 	fmt.Println(computeDisplacement(time))
 
-}
-
-func PromptUserTime() float64 {
-	fmt.Print("Enter Time: ")
-	var timeInp float64
-	_, errAcc := fmt.Scanf("%f", &timeInp)
-	if errAcc != nil {
-		fmt.Println("You did not enter a valid value, replacing it with 0.0")
-		timeInp = 0.0
-	}
-
-	return timeInp
 }
 
 func GenDisplaceFn(acc float64, vel float64, dis float64) func(float64) float64 {
@@ -34,29 +23,20 @@ func GenDisplaceFn(acc float64, vel float64, dis float64) func(float64) float64 
 }
 
 func PromptUserEquation() (float64, float64, float64) {
-	fmt.Print("Enter Acceleration: ")
-	var accInp float64
-	_, errAcc := fmt.Scanf("%f", &accInp)
-	if errAcc != nil {
-		fmt.Println("You did not enter a valid value, replacing it with 0.0")
-		accInp = 0.0
-	}
-
-	fmt.Print("Enter Initial Velocity: ")
-	var velInp float64
-	_, errVel := fmt.Scanf("%f", &velInp)
-	if errVel != nil {
-		fmt.Println("You did not enter a valid value, replacing it with 0.0")
-		velInp = 0.0
-	}
-
-	fmt.Print("Enter Initial displacement: ")
-	var disInp float64
-	_, errDis := fmt.Scanf("%f", &disInp)
-	if errDis != nil {
-		fmt.Println("You did not enter a valid value, replacing it with 0.0")
-		disInp = 0.0
-	}
+	var accInp = funcName("Acceleration")
+	var velInp = funcName("Initial Velocity")
+	var disInp = funcName("Initial Displacement")
 
 	return accInp, velInp, disInp
+}
+
+func funcName(inputName string) float64 {
+	fmt.Printf("Enter %s: ", inputName)
+	var userInput float64
+	_, errAcc := fmt.Scanf("%f", &userInput)
+	if errAcc != nil {
+		fmt.Println("You did not enter a valid value, replacing it with 0.0")
+		userInput = 0.0
+	}
+	return userInput
 }

c880ea208ed10a83252047d3cda2359a04280364
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-05-04 15:12:09 +0100

    Interface Dynamic type and Dynamic value

--- Miscellaneous/Interfaces.go
+++ Miscellaneous/Interfaces.go
@@ -5,17 +5,36 @@ import "fmt"
 type Speaker interface {
 	Speak()
 }
+
+type Cat struct {
+	name string
+}
+
 type Dog struct {
 	name string
 }
 
-func (d Dog) Speak() {
-	fmt.Println(d.name)
+func (c Cat) Speak() {
+	fmt.Println(c.name)
+}
+
+func (d *Dog) Speak() {
+	if d == nil {
+		fmt.Println("<noise>")
+	} else {
+		fmt.Println(d.name)
+	}
 }
 
 func main() {
-	var s1 Speaker
-	var d1 = Dog{"Brian"}
-	s1 = d1
+	var s1, s2 Speaker
+	var c1 = Cat{"Brian"}
+	var d1 *Dog
+	s1 = &c1
+	s2 = d1
 	s1.Speak()
+	// Dynamic
+	if s2 != nil {
+		s2.Speak()
+	}
 }
